273eaa3b721c2db28f570b0c51b56ad9f443ee52
Fitssum Haile <fitsum.wolde@gmail.com>
2017-05-18 10:43:17 -0400

    time feature added and text added in readme document

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fun-Trivia-
+Fun Trivia is a game containing a set of questions to test the knowledge of the user, while having fun playing it. The types of languages used to build this game includes HTML, CSS, JavaScript and JQuery.
+
+The game contains a DOM manipulation by JavaScript and JQuery to validate questions verses their respective answers. Alert feature is also added to indicate whether the player responded correctly or not. Time countdown feature is also displayed which starts counting when the player clicks the start button. Remaining questions are also indicated following clicking of the submit button. 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $( document ).ready(function() {
-  $(".timeRemaining").append("<h3>Time Remaining:</h3>");
-
   var question = [ "Which island is the world's biggest island?", "Which country eats the most chocolate equating to 10 kilos per person per year?", "Which continent is the only continent with no active volcanoes?", "Where is croissant first invented?", "Where are French fries originally from?", "Where is the first sailing boats built?", "Which country is named after a tree?", "Where is paper originated from?", "Which country has the most post offices than any other country (over 100,000)?", "Where is Christmas trees originated from?"];
   var answer = ["Greenland", "Switzerland", "Australia", "Austria", "Belgium", "Egypt", "Brazil", "China", "India", "Germany"];
-  var counter = 0;
-  var remainingQuestion = question[counter].length - 1;
-  $(".questionsRemaining").append("<h3>Questions Remaining: </h3>" + remainingQuestion);
-
+  var counter = 10;
+  var timer = 10;
+  var remainingQuestion = $(questionCounter);
+  var remainingTime = $("#timerInput");
 
   $("#nextQuestion").on("click", function() {
       if(counter < question.length){
@@ -14,16 +12,23 @@ $( document ).ready(function() {
       }
       console.log("random question displayed");
       $("#two").html(question[counter]);
-  })
+
+     var startTimer = setInterval(function() {
+       remainingTime.text(timer);
+       timer-=1;
+     }, 1000)
+     });
 
   $("#submitButton").on("click", function() {
     if ($('#answerBox').val() === answer[counter]){
       console.log("correct alert activated");
       alert("Correct! Go to the next question.");
-      return remainingQuestion;
+      remainingQuestion.html(counter);
+      counter-=1;
     } else {
       alert ("Incorrect answer!");
-      return remainingQuestion;
+      remainingQuestion.html(counter);
+      counter-=1;
     }
   })
 });
